67366c7e77ae88efc6c0a203b420da6e7fc643ce
yueh <purmonth@gmail.com>
2019-10-30 22:50:52 +0800

    add example

--- OpenPrice.py
+++ OpenPrice.py
@@ -16,24 +16,22 @@ training_set = dataset_train.iloc[:, 1:2].values
 sc = MinMaxScaler(feature_range  =(0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
-print(training_set_scaled)
-print("\n")
 #here 60 is the date i predict
 x_train = []  #the date before 60 days
 y_train = []  #the predict date
 for i in range(60, 1258): #1258 is the totals for training
     x_train.append(training_set_scaled[i-60:i,0])
     y_train.append(training_set_scaled[i, 0])
-x_train, y_train = np.array(x_train), np.array(y_train) #trans to numpy array to input RNN
 
-print(x_train)
-print(y_train)
+x_train, y_train = np.array(x_train), np.array(y_train) #trans to numpy array to input RNN
 
 #x_train is two dimension, and reshape to three dimension
 x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1], 1))
-#stock prices, timesteps indicators
-
+print("This is x_train")
 print(x_train)
+print("This is y_train")
+print(y_train)
+#stock prices, timesteps indicators
 
 #Initialisin the RNN
 regressor = Sequential()
@@ -48,13 +46,25 @@ regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 #starting to train
 regressor.fit(x_train, y_train, epochs = 5, batch_size = 32)
 ##to predict
+print("After")
+print("This is X-train")
+print(x_train)
+print("This is y_trian")
+print(y_train)
+
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
 real_stock_price = dataset_test.iloc[:, 1:2].values
 
 dataset_total = pd.concat((dataset_train['Open'],dataset_test['Open']), axis=0)
+print(len(dataset_total))
+print(len(dataset_train))
+print(len(dataset_test))
 inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
+
 inputs = inputs.reshape(-1,1)
+
 inputs = sc.transform(inputs)#Feature Scaling
+
 
 x_test = []
 for i in range(60, 80):
@@ -65,13 +75,7 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 predicted_stock_price = regressor.predict(x_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
- 
-print(real_stock_price)
-print(predicted_stock_price)
-
 test = (real_stock_price-predicted_stock_price)/predicted_stock_price
-print(test)
-
 
 plt.plot(real_stock_price, color = 'red', label = 'Real Google Stock Price')  
 plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Google Stock Price')  
@@ -80,4 +84,3 @@ plt.xlabel('Time')
 plt.ylabel('Google Stock Price')
 plt.legend()
 plt.show()
-
