8a2e2399157456563195d2c9212b058dd34dcf8b
purmonth <purmonth@gmail.com>
2019-11-29 00:06:18 +0800

    complete 0050 predict high price

--- LoadData.py
+++ LoadData.py
@@ -6,20 +6,19 @@ import csv
 import os
 
 li = []
-df = pd.read_json("json/2014-02-11.json",orient = 'index')
+df = pd.read_json("json/2019-01-02.json",orient = 'index')
 df = pd.DataFrame(df,index=["0050"],columns=['high'])
 number = 0
 
-filenames = glob.glob("json/2014-02-*.json")
+filenames = glob.glob("json/2019-*.json")
 for filename in filenames:
     df1 = pd.read_json(filename,orient = 'index')
     df1 = pd.DataFrame(df1,index=["0050"],columns=['high'])
-    print(df1)
     df = pd.DataFrame(pd.np.append(df, df1), columns=['0050_high'])
+    print(filename)
 
 
 print(df)
-##print(li)
-df.to_csv('output.csv',header=0,index=0)
+df.to_csv('0050_2019_high.csv',header=0,index=0)
 
 ##pd.DataFrame(pd.np.append(data1.values, data2.values), columns=['A'])
