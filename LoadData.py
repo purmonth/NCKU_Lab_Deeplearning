cc2d3c324f8170817a321aec9ae3070068a4c949
purmonth <purmonth@gmail.com>
2019-12-06 10:39:01 +0800

    change folder

--- LoadData.py
+++ LoadData.py
@@ -6,19 +6,23 @@ import csv
 import os
 
 li = []
-df = pd.read_json("json/2019-01-02.json",orient = 'index')
-df = pd.DataFrame(df,index=["0050"],columns=['open'])
+df = pd.DataFrame(columns=['close', 'date'])
+##df = pd.read_json("json/2019-01-02.json",orient = 'index')
+##df = pd.DataFrame(df,index=["0050"],columns=['close'])
 number = 0
 
-filenames = glob.glob("json/*.json")
+filenames = glob.glob("json/2019-01-0*.json")
 for filename in filenames:
     df1 = pd.read_json(filename,orient = 'index')
-    df1 = pd.DataFrame(df1,index=["0050"],columns=['open'])
-    df = pd.DataFrame(pd.np.append(df, df1), columns=['0050_open'])
-    print(filename)
+    ##print(df1)
+    df1 = pd.DataFrame(df1,index=["0050"],columns=['close'])
+    filename = filename.replace(".json","")
+    df1['date'] = filename.replace("json/","")
+    print(df1)
+    df = df.append(df1)
 
 
 print(df)
-df.to_csv('0050_2004_open.csv',header=0,index=0)
+df.to_csv('test.csv',header=0,index=0)
 
 ##pd.DataFrame(pd.np.append(data1.values, data2.values), columns=['A'])
